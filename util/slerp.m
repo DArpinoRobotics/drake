44be2ad7576e506e0d251ce2b670e2fb25827e04
Robin Deits <robin.deits@gmail.com>
2014-09-04 21:23:35 -0400

    highly simplified swing plan reasoning only about the foot orientation

---
+++ util/slerp.m
@@ -0,0 +1,11 @@
+function out = slerp(in1,in2,t)
+  % Copied from drc/software/perception/matt_sandbox/matlab/multisense_calib
+in1 = in1/norm(in1);
+in2 = in2/norm(in2);
+angle = abs(acos(dot(in1,in2)));
+sin_angle = sin(angle);
+if (sin_angle==0)
+    out = in1;
+else
+    out = sin((1-t)*angle)/sin(angle)*in1 + sin(t*angle)/sin(angle)*in2;
+end
