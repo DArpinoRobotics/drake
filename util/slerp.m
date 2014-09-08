0d4eb97d1170aa10000eb97ec25515c9a9d53557
Robin Deits <robin.deits@gmail.com>
2014-09-08 14:00:48 -0400

    fix slerp sometimes taking the long way around

--- util/slerp.m
+++ util/slerp.m
@@ -1,11 +1,16 @@
 function out = slerp(in1,in2,t)
-  % Copied from drc/software/perception/matt_sandbox/matlab/multisense_calib
+  % Copied from
+  % drc/software/perception/matt_sandbox/matlab/multisense_calib by Matthew
+  % Antone
 in1 = in1/norm(in1);
 in2 = in2/norm(in2);
-angle = abs(acos(dot(in1,in2)));
+if dot(in1,in2) < 0
+  in2 = -in2;
+end
+angle = acos(dot(in1, in2));
 sin_angle = sin(angle);
 if (sin_angle==0)
     out = in1;
 else
-    out = sin((1-t)*angle)/sin(angle)*in1 + sin(t*angle)/sin(angle)*in2;
+    out = sin((1-t)*angle)/sin_angle*in1 + sin(t*angle)/sin_angle*in2;
 end
